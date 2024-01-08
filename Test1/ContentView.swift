880cb2c706588a141896e4ab4187ae91952ddb31
pjexaf <106393@gmail.com>
2024-01-08 17:55:18 +0100

    Initial Commit

---
+++ Test1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test1
+//
+//  Created by Roman Glukhov on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Roman Glukhov on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
