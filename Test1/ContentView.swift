811b48b53b706de5fd05175ff8d955fdfd0a52a8
pjexaf <106393@gmail.com>
2024-01-16 12:54:33 +0100

    Stanford CS193p 2023 Initial  CardView, card toggling

--- Test1/ContentView.swift
+++ Test1/ContentView.swift
@@ -9,15 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView(isFaceUp: true)
         }
         .padding()
+        .foregroundColor(.orange)
     }
 }
+
+struct CardView: View {
+    @State var isFaceUp = false;
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if (isFaceUp) {
+                base.strokeBorder(lineWidth: 6)
+                base.foregroundColor(.white)
+                VStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                        .foregroundColor(Color.red)
+                }
+            } else {
+                base.fill(.orange)
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
